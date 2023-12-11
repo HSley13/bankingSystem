0128fd63c0c8b8d6445e6abb0b7d728ece0fe141
Sley HORTES <sleyhortes13@gmail.com>
2023-12-11 09:09:54 +0800

    creating the main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <array>
+#include <iterator>
+using namespace std;
+
+int main(void)
+{
+
+
+
+
+
+}
